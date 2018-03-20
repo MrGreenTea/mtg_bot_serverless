8e7f7a482984e6faca86fc08900550b9a5ccc404
Jonas Bulik <jonas.bulik@netqa.de>
2018-03-20 17:27:19 +0100

    cleaned up a bit more

--- testing/profile.py
+++ testing/profile.py
@@ -1,7 +1,7 @@
-from src.scryfall import compute_answer
+from src import scryfall
 
 msg = {'id': '535717811308309727',
        'from': {'id': 124731522, 'is_bot': False, 'first_name': 'Jonas', 'username': 'mrgreentea',
                 'language_code': 'en-US'}, 'query': 'Huatli', 'offset': ''}
 
-compute_answer(msg)
+scryfall.compute_answer(scryfall.glance_msg(msg))
