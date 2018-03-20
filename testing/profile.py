9b0d4cb599328371057e7140585be919e57118fa
Jonas Bulik <jonas.bulik@netqa.de>
2018-03-20 14:46:12 +0100

    added testing and pylint

---
+++ testing/profile.py
@@ -0,0 +1,7 @@
+from handler import compute_answer
+
+msg = {'id': '535717811308309727',
+       'from': {'id': 124731522, 'is_bot': False, 'first_name': 'Jonas', 'username': 'mrgreentea',
+                'language_code': 'en-US'}, 'query': 'Huatli', 'offset': ''}
+
+compute_answer(msg)
---
+++ testing/tests.py
@@ -0,0 +1,18 @@
+from hypothesis import given, strategies
+
+import handler
+
+
+def test_inline_card():
+    card = {
+        'name': 'name',
+        'id': 'id',
+        'scryfall_uri': 'scryfall_uri'
+            }
+    inline_card = handler.inline_card(card, 0, 0, 'photo_url', 'thumb_url')
+    assert inline_card['type'] == 'photo'
+    assert inline_card['photo_width'] == 0
+    assert inline_card['photo_height'] == 0
+    assert inline_card['photo_url'] == 'photo_url'
+    assert inline_card['thumb_url'] == 'thumb_url'
+    assert inline_card['id'] == 'idname'
